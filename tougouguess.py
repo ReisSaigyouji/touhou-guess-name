37713fb30d86649770926939a2bd81e0b70e6b32
Reis Saigyouji <olegride22@gmail.com>
2025-11-02 04:36:20 +0100

    remaking it all completely to have a gui

--- tougouguess.py
+++ tougouguess.py
@@ -1,53 +1,22 @@
 import random
+import tkinter as tk
 
 names = ["REIMU", "MARISA", "RUMIA", "CIRNO", "DAIYOUSEI", "MEILING",
          "KOAKUMA", "PATCHOULI", "SAKUYA", "REMILIA", "FLANDRE"]
 
-def game():
-    name = random.choice(names)
-    guess = '_' * len(name)
-    used = []
-    print("Guess the name, it has", len(name), "letters.")
-    while guess != name:
-        print(guess)
-        letter = input("Guess a letter: ").upper()
-        print(letter)
-        if letter in used:
-            print("You already tried that letter!")
-        elif len(letter) != 1:
-            print("Please enter a single letter.")
-        elif letter in name:
-            new = ""
-            print("Correct!")
-            for i in range(len(name)):
-                if letter == name[i]:
-                    new += letter
-                else:
-                    new += guess[i]
-            guess = new
-            used.append(letter)
-        else:
-            print("There is no such letter!")
-            used.append(letter)
-    print(f"That's right! It's {name}")
+class GameGUI:
+    def __init__(self, root):
+        self.root = root
+        root.title("Touhou Guessing Game")
+
+        self.name = random.choice(names)
+        self.guess = "_" * len(self.name)
+        # state of word
+        self.word_label = tk.Label(root, text=" ".join(self.guess), font=("Arial", 24))
+        self.word_label.pack(pady = 10)
+        # input
+        self.entry = tk.Entry(root, font=("Arial", 18), width=5)
+        self.entry.pack()
+        self.entry.focus()
 
 
-def main():
-    choice = ''
-    while choice != '0':
-        choice = input("""Welcome to character guessing game!
-        Choose one of the options:
-        Play - 1
-        Exit - 0
-        Your choice: """)
-        if choice == '1':
-            game()
-        elif choice == '0':
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid input.")
-
-
-if __name__ == '__main__':
-    main()
